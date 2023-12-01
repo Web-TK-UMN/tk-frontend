20797c80ede30794cd12076af98b3b868146a5f9
rdhwan <fathanridhwann@gmail.com>
2023-12-01 16:11:30 +0700

    feat: mouse scroll down animation (later use)

---
+++ src/components/ScrollDownAnim.tsx
@@ -0,0 +1,40 @@
+import { Box, Stack } from "@chakra-ui/react";
+import { motion } from "framer-motion";
+
+const ScrollDownAnim = () => {
+  return (
+    // mouse scroll down anim using chakra-ui and framer-motion
+    <Box
+      w={"1.5em"}
+      h={"2.5em"}
+      border={"solid 2px white"}
+      rounded={"full"}
+      //   align={"center"}
+      //   justify={"center"}
+    >
+      <motion.div
+        animate={{
+          y: [0, 10],
+        }}
+        transition={{
+          duration: 1,
+          repeat: Infinity,
+          repeatType: "loop",
+          ease: "easeOut",
+        }}
+      >
+        <Box
+          w={"0.25em"}
+          h={"0.5em"}
+          bgColor={"white"}
+          rounded={"full"}
+          pos={"relative"}
+          top={"0.5em"}
+          left={"0.5em"}
+        />
+      </motion.div>
+    </Box>
+  );
+};
+
+export default ScrollDownAnim;
