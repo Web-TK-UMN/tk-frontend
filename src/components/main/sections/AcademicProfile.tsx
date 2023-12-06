e83348138e4aae8522010c1171c6a002c0d5764e
rdhwan <fathanridhwann@gmail.com>
2023-12-06 22:47:48 +0700

    fix: adjustment for academic and statistic section

--- src/components/main/sections/AcademicProfile.tsx
+++ src/components/main/sections/AcademicProfile.tsx
@@ -1,49 +1,105 @@
-import { Stack, Image, Text } from "@chakra-ui/react";
+import { MotionStack } from "@/components/ChakraFramer";
+import { Stack, Image, Text, VisuallyHidden } from "@chakra-ui/react";
 
 const AcademicProfile = () => {
   return (
-    <Stack direction={"column"} align={"center"}>
-      <Stack
-        direction={"column"}
-        align={"center"}
-        justify={"top"}
-        w={"85%"}
-        mt={"2em"}
+    <Stack
+      id="academic"
+      direction={"column"}
+      align={"center"}
+      px={["2em", "2em", "6em", "6em", "6em"]}
+      pt={"4em"}
+      pb={"2em"}
+    >
+      <MotionStack
+        flexDir={"column"}
+        alignItems={"center"}
+        justifyContent={"top"}
+        w={"full"}
+        viewport={{ once: true }}
+        initial={{
+          opacity: 0,
+          y: -10,
+        }}
+        whileInView={{
+          opacity: 1,
+          y: 0,
+        }}
+        // @ts-expect-error chakra ui typescript error
+        transition={{
+          duration: 1,
+          ease: "easeOut",
+          delay: 0.5,
+        }}
       >
+        <VisuallyHidden>What is Computer Engineering?</VisuallyHidden>
         <Image src="/assets/AcademicProfile.svg" />
-      </Stack>
-      <Stack
-        direction={"row"}
-        justify={"center"}
-        w={"85%"}
+      </MotionStack>
+      <MotionStack
+        flexDir={"row"}
+        justifyContent={"center"}
+        w={"full"}
         h={["1.5em", "2em", "2.5em", "2.5em", "2.5em"]}
+        my={["1em", "1em", "2em", "2em", "2em"]}
+        gap={"1em"}
+        viewport={{ once: true }}
+        initial={{
+          opacity: 0,
+          y: -10,
+        }}
+        whileInView={{
+          opacity: 1,
+          y: 0,
+        }}
+        // @ts-expect-error chakra ui typescript error
+        transition={{
+          duration: 1,
+          ease: "easeOut",
+          delay: 0.5,
+        }}
       >
         <Stack
-          direction={"row"}
           rounded={["md", "xl", "xl", "xl", "xl"]}
-          w={["40%", "35%", "25%", "25%", "20%"]}
+          w={"fit-content"}
           bgColor={"#FD8E09"}
           align={"center"}
           justify={"center"}
+          px={"1em"}
         >
           <Text
             textColor={"white"}
-            fontSize={["xx-small", "xx-small", "small", "medium", "large"]}
+            fontSize={["md", "md", "md", "md", "lg"]}
+            whiteSpace={"nowrap"}
           >
             Academic Profile
           </Text>
         </Stack>
         <Stack
-          direction={"row"}
           rounded={["md", "xl", "xl", "xl", "xl"]}
-          w={"80%"}
+          w={"full"}
           bgColor={"#003C72"}
         ></Stack>
-      </Stack>
-      <Stack direction={"row"} w={"85%"}>
+      </MotionStack>
+      <MotionStack
+        viewport={{ once: true }}
+        initial={{
+          opacity: 0,
+          y: -10,
+        }}
+        whileInView={{
+          opacity: 1,
+          y: 0,
+        }}
+        // @ts-expect-error chakra ui typescript error
+        transition={{
+          duration: 1,
+          ease: "easeOut",
+          delay: 0.5,
+        }}
+      >
         <Text
           textColor={"#676767"}
-          fontSize={["xx-small", "xx-small", "small", "medium", "large"]}
+          // fontSize={["xx-small", "xx-small", "small", "medium", "large"]}
           textAlign={"justify"}
         >
           The Computer Engineering program is designed to produce the graduates
@@ -53,12 +109,7 @@ const AcademicProfile = () => {
           and data communication both in a company and in a mobile format and
           also design various digital-technology-based devices.
         </Text>
-      </Stack>
-      <Image
-        mt={["4em", "4em", "8em", "8em", "8em"]}
-        w={"85%"}
-        src="/assets/HorizontalLine.svg"
-      />
+      </MotionStack>
     </Stack>
   );
 };
--- src/components/main/sections/Statistic.tsx
+++ src/components/main/sections/Statistic.tsx
@@ -1,138 +1,201 @@
-import { Stack, Text, Image } from "@chakra-ui/react";
+import { MotionImage, MotionStack } from "@/components/ChakraFramer";
+import { Stack, Text, Image, Heading, Flex } from "@chakra-ui/react";
 
 const Statistic = () => {
   return (
-    <Stack direction={"column"} align={"center"} w={"100%"}>
-      {" "}
-      <Stack
-        direction={["column", "column", "row", "row", "row"]}
-        w={"100%"}
-        align={"center"}
-        justify={"center"}
-        mt={"2em"}
+    <Stack
+      id="statistic"
+      direction={"column"}
+      px={["2em", "2em", "6em", "6em", "6em"]}
+      pt={"2em"}
+      pb={"4em"}
+      align={"center"}
+      justify={"center"}
+    >
+      <MotionImage
+        src="/assets/HorizontalLine.svg"
+        viewport={{ once: true }}
+        initial={{
+          opacity: 0,
+          y: -10,
+        }}
+        whileInView={{
+          opacity: 1,
+          y: 0,
+        }}
+        // @ts-expect-error chakra ui typescript error
+        transition={{
+          duration: 1,
+          ease: "easeOut",
+          delay: 0.5,
+        }}
+      />
+
+      <MotionStack
+        flexDir={"column"}
+        my={"2em"}
+        alignItems={"center"}
+        justifyContent={"center"}
+        viewport={{ once: true }}
+        initial={{
+          opacity: 0,
+          y: -10,
+        }}
+        whileInView={{
+          opacity: 1,
+          y: 0,
+        }}
+        // @ts-expect-error chakra ui typescript error
+        transition={{
+          duration: 1,
+          ease: "easeOut",
+          delay: 0.5,
+        }}
       >
-        <Stack
-          direction={"row"}
-          align={"center"}
-          justify={"center"}
-          fontSize={["small", "medium", "large", "large", "x-large"]}
-          fontWeight={600}
-        >
-          <Text>we are continuously refining and </Text>
-        </Stack>
-        <Stack direction={"row"} align={"center"} justify={"center"}>
-          <Stack direction={"row"} align={"center"} justify={"center"}>
-            <Text
-              fontSize={["small", "medium", "large", "large", "x-large"]}
-              fontWeight={600}
-            >
-              enhancing process for
-            </Text>
-          </Stack>
-          <Stack
-            direction={"row"}
-            align={"center"}
-            justify={"center"}
-            textColor={"#003C72"}
+        <Heading size={"md"} fontWeight={"medium"} textAlign={"center"}>
+          we are continuously refining and enhancing process for{" "}
+          <Heading
+            as={"span"}
+            size={"md"}
+            fontWeight={"semibold"}
+            color={"#003C72"}
           >
-            <Text
-              fontSize={["small", "medium", "large", "large", "x-large"]}
-              fontWeight={600}
-            >
-              better results.
-            </Text>
-          </Stack>
-        </Stack>
-      </Stack>
-      <Stack direction={"row"} w={"85%"} justify={"space-between"} mt={"2em"}>
-        <Stack direction={"column"}>
-          <Image src="/assets/GPA.svg" />
-          <Stack direction={"column"} align={"end"} mt={"-30%"} px={"1em"}>
-            <Text
-              fontWeight={"bold"}
-              fontSize={[
-                "large",
-                "large",
-                "xx-large",
-                "xxx-large",
-                "xxx-large",
-              ]}
-              textColor={"#555555"}
+            better results.
+          </Heading>
+        </Heading>
+      </MotionStack>
+
+      <Stack
+        w={"full"}
+        gap={"1em"}
+        direction={["column", "column", "row", "row", "row"]}
+        my={"2em"}
+      >
+        <MotionStack
+          pos={"relative"}
+          h={["18em", "20em", "14em", "16em", "24em"]}
+          viewport={{ once: true }}
+          initial={{
+            opacity: 0,
+            y: -10,
+          }}
+          whileInView={{
+            opacity: 1,
+            y: 0,
+          }}
+          // @ts-expect-error chakra ui typescript error
+          transition={{
+            duration: 1,
+            ease: "easeOut",
+            delay: 0.5,
+          }}
+        >
+          <Image
+            src={"/assets/GPA.svg"}
+            h={["18em", "20em", "14em", "16em", "24em"]}
+          />
+          <Stack
+            pos={"absolute"}
+            w={"full"}
+            h={"full"}
+            align={"end"}
+            justify={"end"}
+            px={["1em", "1em", "2em", "2em", "2em"]}
+            py={["3em", "5em", "2em", "2em", "2em"]}
+          >
+            <Heading
+              fontSize={["4em", "5em", "5em", "5em", "6em"]}
+              fontWeight={"semibold"}
             >
               3.14
-            </Text>
-            <Text
-              fontSize={["x-small", "medium", "large", "large", "x-large"]}
-              textColor={"#555555"}
-            >
-              student's average GPA
-            </Text>
+            </Heading>
+            <Text fontWeight={"medium"}>student's average GPA</Text>
           </Stack>
-        </Stack>
-        <Stack direction={"column"}>
-          <Stack direction={"column"}>
-            <Image src="/assets/Employment.svg" />
-            <Stack direction={"column"} align={"end"}>
-              <Stack direction={"row"} align={"center"} mt={"-25%"} px={"1em"}>
-                <Text
-                  fontSize={[
-                    "large",
-                    "large",
-                    "xx-large",
-                    "xxx-large",
-                    "xxx-large",
-                  ]}
-                  fontWeight={"bold"}
-                  textColor={"#555555"}
-                >
-                  3
-                </Text>
-                <Text
-                  fontSize={[
-                    "medium",
-                    "medium",
-                    "x-large",
-                    "xx-large",
-                    "xx-large",
-                  ]}
-                  fontWeight={"bold"}
-                  textColor={"#555555"}
-                >
-                  Months
-                </Text>
-              </Stack>
-              <Stack direction={"row"} align={"center"} px={"1em"}>
-                <Text
-                  textAlign={"justify"}
-                  fontSize={["x-small", "medium", "small", "large", "x-large"]}
-                  textColor={"#555555"}
-                >
-                  average waiting time from graduation to getting the first job
-                </Text>
-              </Stack>
-            </Stack>
-          </Stack>
-          <Stack direction={"column"}>
-            <Image src="/assets/Rectangle.svg" />
-            <Stack direction={"column"} align={"end"} mt={"-10%"} px={"1em"}>
-              <Text
-                fontSize={["x-small", "medium", "small", "large", "x-large"]}
-                textColor={"#555555"}
-              >
-                i don't know
+        </MotionStack>
+
+        <Stack
+          direction={"column"}
+          flex={1}
+          gap={["2em", "2em", "1em", "1em", "1em"]}
+        >
+          <MotionStack
+            h={"75%"}
+            bgColor={"#E5E5E5"}
+            p={["1em", "2em", "2em", "2em", "2em"]}
+            rounded={"2xl"}
+            flexDir={"column"}
+            justifyContent={"space-between"}
+            viewport={{ once: true }}
+            initial={{
+              opacity: 0,
+              y: -10,
+            }}
+            whileInView={{
+              opacity: 1,
+              y: 0,
+            }}
+            // @ts-expect-error chakra ui typescript error
+            transition={{
+              duration: 1,
+              ease: "easeOut",
+              delay: 0.5,
+            }}
+          >
+            <Image src="/assets/calendar.svg" boxSize={"4em"}></Image>
+            <Stack flex={1} align={"end"} justify={"end"}>
+              <Heading fontSize={["2em", "3em", "3em", "3em", "4em"]}>
+                3 <Heading as={"span"}>Months</Heading>
+              </Heading>
+              <Text fontWeight={"medium"} textAlign={"end"}>
+                average waiting time from graduation to getting the first job
               </Text>
             </Stack>
-          </Stack>
+          </MotionStack>
+          <MotionStack
+            bgColor={"#E5E5E5"}
+            p={["1em", "2em", "2em", "2em", "2em"]}
+            rounded={"2xl"}
+            alignItems={"end"}
+            justifyContent={"center"}
+            viewport={{ once: true }}
+            initial={{
+              opacity: 0,
+              y: -10,
+            }}
+            whileInView={{
+              opacity: 1,
+              y: 0,
+            }}
+            // @ts-expect-error chakra ui typescript error
+            transition={{
+              duration: 1,
+              ease: "easeOut",
+              delay: 0.5,
+            }}
+          >
+            <Text fontWeight={"medium"}>No content here</Text>
+          </MotionStack>
         </Stack>
       </Stack>
-      <Stack direction={"column"} w={"85%"} align={"center"}>
-        <Image
-          src="/assets/HorizontalLine.svg"
-          w={"100%"}
-          mt={"8em"}
-          transform="scaleY(-1)"
-        />
-      </Stack>
+
+      <MotionImage
+        src="/assets/HorizontalLine.svg"
+        transform={"scaleY(-1)"}
+        initial={{
+          opacity: 0,
+          y: -10,
+        }}
+        whileInView={{
+          opacity: 1,
+          y: 0,
+        }}
+        // @ts-expect-error chakra ui typescript error
+        transition={{
+          duration: 1,
+          ease: "easeOut",
+          delay: 0.5,
+        }}
+      />
     </Stack>
   );
 };
